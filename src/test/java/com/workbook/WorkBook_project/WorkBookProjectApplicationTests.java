9b82186c158af4a34e3c50723967dd17e94b3963
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-04-30 16:49:07 -0300

    first commit

---
+++ src/test/java/com/workbook/WorkBook_project/WorkBookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workbook.WorkBook_project;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
